f692f989bfaa876eb0531f694b81848772ae1303
Mohammed Es sabtaouy <simsay04@hotmail.fr>
2020-02-18 13:24:18 +0100

    Add project files.

---
+++ US-Wahl/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace US_Wahl
+{
+    enum Geschlecht { Weiblich, Mänlich }
+    enum Beeinflußbarkeit { Leicht, Mittel, Schwer }
+    enum Schicht { UnterSchicht, UntermittelSchicht, OberemittelSchicht, ObereSchicht }
+    enum PolitischeHeimat { Republikaner, Demokraten }
+    class Person
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Nachname { get; set; }
+        public  Geschlecht  geschlecht { get; set; }
+        public Beeinflußbarkeit  beeinflußbarkeit { get; set; }
+        public Schicht schicht { get; set; }
+    }
+}
